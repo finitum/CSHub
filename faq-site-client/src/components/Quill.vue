0000de73107b41b11d6f5f02794d4a95aa0d54ef
RobbinBaauw <robbin.baauw@gmail.com>
2018-10-07 19:57:18 +0200

    Fixed all linting and ts errors

--- faq-site-client/src/components/Quill.vue
+++ faq-site-client/src/components/Quill.vue
@@ -13,23 +13,18 @@
 <script lang="ts">
   import Vue from "vue";
 
-  // require styles for quill
+  import JQuery from "jquery";
+
   import "quill/dist/quill.core.css";
   import "quill/dist/quill.snow.css";
   import "quill/dist/quill.bubble.css";
-
-  import JQuery from "jquery";
-
   import Quill from "quill";
-
-  import {mathquill} from "../plugins/mathquill4quill/mathquill"
-  import "../plugins/mathquill4quill/mathquill.css"
-
-
-  import "katex/dist/katex.min.css"
-  import katex from "katex/dist/katex.min.js"
-
-  import {mathquill4quill} from "../plugins/mathquill4quill/mathquill4quill"
+  import {mathquill} from "../plugins/quill/mathquill";
+  import "../plugins/quill/mathquill.css";
+  import "katex/dist/katex.min.css";
+  // @ts-ignore
+  import katex from "katex/dist/katex.min";
+  import {mathquill4quill} from "../plugins/quill/mathquill4quill";
 
   export default Vue.extend({
       name: "Quill",
@@ -43,21 +38,21 @@
                   modules: {
                       formula: true,
                       toolbar: [
-                          ['bold', 'italic', 'underline', 'strike'],
-                          ['blockquote', 'code-block'],
-                          [{ 'header': 1 }, { 'header': 2 }],
-                          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                          [{ 'script': 'sub' }, { 'script': 'super' }],
-                          [{ 'indent': '-1' }, { 'indent': '+1' }],
-                          [{ 'direction': 'rtl' }],
-                          [{ 'size': ['small', false, 'large', 'huge'] }],
-                          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                          [{ 'color': [] }, { 'background': [] }],
-                          [{ 'font': [] }],
-                          [{ 'align': [] }],
-                          ['clean'],
-                          ['link', 'image'],
-                          ['formula']
+                          ["bold", "italic", "underline", "strike"],
+                          ["blockquote", "code-block"],
+                          [{ header: 1 }, { header: 2 }],
+                          [{ list: "ordered" }, { list: "bullet" }],
+                          [{ script: "sub" }, { script: "super" }],
+                          [{ indent: "-1" }, { indent: "+1" }],
+                          [{ direction: "rtl" }],
+                          [{ size: ["small", false, "large", "huge"] }],
+                          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                          [{ color: [] }, { background: [] }],
+                          [{ font: [] }],
+                          [{ align: [] }],
+                          ["clean"],
+                          ["link", "image"],
+                          ["formula"]
                       ]
                   }
               }
@@ -81,32 +76,30 @@
           this.initQuill();
       },
       methods: {
-          initRequirements(){
-              (window as any)["jQuery"] = JQuery;
-              (window as any)["katex"] = katex;
+          initRequirements() {
+              (window as any).jQuery = JQuery;
+              (window as any).katex = katex;
 
               mathquill();
-              mathquill4quill((Quill as any), (window as any)["MathQuill"]);
+              mathquill4quill((Quill as any), (window as any).MathQuill);
           },
-          initQuill(){
+          initQuill() {
               // Options
               this._options = Object.assign({}, this.defaultOptions, this.options);
 
               this.editor = new Quill("#editor", this._options);
-              this.editor.enableMathQuillFormulaAuthoring();
+              (this.editor as any).enableMathQuillFormulaAuthoring();
 
-              this.editor.enable(false);
+              (this.editor as any).enable(false);
 
               // Set content
               if (this.value || this.content) {
-                  this.editor.pasteHTML(this.value || this.content)
+                  (this.editor as any).pasteHTML(this.value || this.content);
               }
 
-              if(!this.disabled){
-                  this.editor.enable(true)
+              if (!this.disabled) {
+                  (this.editor as any).enable(true);
               }
-
-
           }
       }
   });
--- faq-site-client/src/components/posts/PostPreview.vue
+++ faq-site-client/src/components/posts/PostPreview.vue
@@ -34,7 +34,7 @@
         data() {
             return {
                 post: null as IPostReduced
-            }
+            };
         },
         mounted() {
             // IntelliJ doesn't know postId, but it is there. I'm afraid that's the limited vue TS implementation (https://github.com/vuejs/vue/pull/6856 might help)
@@ -44,7 +44,7 @@
                 LogObjectConsole(callbackData.post, "PostPreview");
             });
         }
-    })
+    });
 </script>
 
 <style scoped>
