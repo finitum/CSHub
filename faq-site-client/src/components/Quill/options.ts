e47f13555aa7ef0e7335cf4609154e034dbcccea
Victor Roest <victor@xirion.net>
2018-10-08 10:17:09 +0200

    Split up default options into separate file

---
+++ faq-site-client/src/components/Quill/options.ts
@@ -0,0 +1,24 @@
+export default {
+    theme: "snow",
+    placeholder: "Type here ...",
+    modules: {
+        formula: true,
+        toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            [{ direction: "rtl" }],
+            [{ size: ["small", false, "large", "huge"] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ["clean"],
+            ["link", "image"],
+            ["formula"]
+        ]
+    }
+};
