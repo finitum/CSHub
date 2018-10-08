c5d419d754d6300bbee0873b3dd5ad31561a028a
Victor Roest <victor@xirion.net>
2018-10-08 22:31:16 +0200

    Beter typing
    Signed-off-by: Victor Roest <victor@xirion.net>

--- faq-site-client/src/store/data/mutations.ts
+++ faq-site-client/src/store/data/mutations.ts
@@ -1,10 +1,12 @@
 import {IDataState} from "./state";
 import {IPost, ITopic} from "../../../../faq-site-shared/models";
+// @ts-ignore
+import { Delta } from "quill-delta";
 
 export const setTopics = (state: IDataState, payload: ITopic[]) => {
     state.topics = payload;
 };
 
-export const setQuillContents = (state: IDataState, payload: object[]) => {
+export const setQuillContents = (state: IDataState, payload: Delta) => {
     state.quillContents = payload;
 };
