5edc51eb708244ab2841c7b49804901a27923c69
Robbin Baauw <robbin.baauw@gmail.com>
2018-10-10 16:41:19 +0200

    Fixed the unfixed toolbar so the toolbar is fixed

--- faq-site-client/src/components/global/Toolbar.vue
+++ faq-site-client/src/components/global/Toolbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-toolbar color="primary" app absolute clipped-left>
+    <v-toolbar color="primary" app fixed clipped-left>
         <v-toolbar-side-icon @click.native="drawerComputed = !drawerComputed"></v-toolbar-side-icon>
         <div class="title ml-3 mr-5" @click="routeHome()">CS&nbsp;<span class="font-weight-light">Hub</span></div>
         <v-text-field
