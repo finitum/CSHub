986653bb517252df87bef2d091fb630d62051534
Victor Roest <victor@xirion.net>
2018-10-13 11:30:06 +0200

    Fixed error caused by merge
    Signed-off-by: Victor Roest <victor@xirion.net>

--- faq-site-client/src/components/global/NavDrawer.vue
+++ faq-site-client/src/components/global/NavDrawer.vue
@@ -19,7 +19,7 @@
                 </v-flex>
             </v-layout>
             <router-link :to="navigationLocations.USERDASHBOARD" v-if="userLoggedInComputed"><NavDrawerItem icon="mdi-account" text="User dashboard"></NavDrawerItem></router-link>
-            <router-link :to="navigationLocations.ADMINDASHBOARD" v-if="userLoggedInComputed && userIsAdminComputed"><NavDrawerItem icon="mdi-account-supervisor" text="Admin dashboard"></NavDrawerItem></router-link>
+            <router-link :to="navigationLocations.ADMINDASHBOARD" v-if="userLoggedInComputed && userAdminComputed"><NavDrawerItem icon="mdi-account-supervisor" text="Admin dashboard"></NavDrawerItem></router-link>
             <router-link :to="navigationLocations.LOGIN" v-if="!userLoggedInComputed"><NavDrawerItem icon="mdi-login" text="Login"></NavDrawerItem></router-link>
             <v-divider dark class="my-3"></v-divider>
             <v-layout
