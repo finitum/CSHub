3daceecabb604d66be76d837affffab97f21a61a
RobbinBaauw <robbin.baauw@gmail.com>
2018-10-14 21:25:59 +0200

    User table up and running, logic will be for 0.2.0

--- faq-site-server/src/components/pages/admin/adminDashboard.ts
+++ faq-site-server/src/components/pages/admin/adminDashboard.ts
@@ -1,30 +1,47 @@
 import {app, logger} from "../../../";
-import {GetAllUsersRequest} from "../../../../../faq-site-shared/api-calls/admin";
+import {GetAllUsersCallBack, GetAllUsersRequest} from "../../../../../faq-site-shared/api-calls/admin";
 import {Request, Response} from "express";
 import {checkTokenValidity} from "../../../auth/middleware";
-import {DatabaseResultSet, query} from "../../../database-connection";
+import {DatabaseResultSet, query} from "../../../utilities/database-connection";
 import {IUser} from "../../../../../faq-site-shared/models";
 
-app.get(GetAllUsersRequest.getURL, (req: Request, res: Response) => {
-    const GetAllUsersRequest = req.body as GetAllUsersRequest;
+app.post(GetAllUsersRequest.getURL, (req: Request, res: Response) => {
+    const getAllUsersRequest = req.body as GetAllUsersRequest;
 
     const token = checkTokenValidity(req);
 
     // Check if token is valid and user is admin
-    if(token.valid && token.tokenObj.user.admin) {
+    if (token.valid && token.tokenObj.user.admin) {
+        query(`
+        SELECT id, email, firstname, lastname, blocked, verified, admin, avatar
+        FROM users
+        LIMIT ?, ?
+        `, ((getAllUsersRequest.page - 1) * getAllUsersRequest.rowsPerPage), getAllUsersRequest.rowsPerPage)
+            .then((data: DatabaseResultSet) => {
 
+                const users: IUser[] = [];
+
+                for (const userRow of data.convertRowsToResultObjects()) {
+                    users.push({
+                        id: userRow.getNumberFromDB("id"),
+                        admin: userRow.getNumberFromDB("admin") === 1,
+                        firstname: userRow.getStringFromDB("firstname"),
+                        lastname: userRow.getStringFromDB("lastname"),
+                        avatar: userRow.getStringFromDB("avatar"),
+                        email: userRow.getStringFromDB("email"),
+                        blocked: userRow.getNumberFromDB("blocked") === 1,
+                        verified: userRow.getNumberFromDB("verified") === 1
+                    });
+                }
+
+                query(`
+                    SELECT COUNT(*) as count
+                    FROM users
+                `)
+                    .then((countResult: DatabaseResultSet) => {
+                        const count = countResult.getNumberFromDB("count");
+                        res.json(new GetAllUsersCallBack(users, count));
+                    });
+            });
     }
 });
-
-export const getAllUsers = (): Promise<IUser[] | null> => {
-    query(`
-        SELECT id,email,admin,created,blocked,verified,firstname,lastname
-        FROM users
-    `).then((users: DatabaseResultSet) => {
-
-        const usersParsed: IUser[] = [];
-
-    });
-
-    return;
-};
--- utilities/queries/QUERIES.md
+++ utilities/queries/QUERIES.md
@@ -1,5 +1,6 @@
 To view executed queries, execute the following query:
 
-SELECT CONVERT(argument USING utf8)
+SELECT CONVERT(argument USING utf8), event_time
 FROM mysql.general_log
 WHERE event_time > NOW() - INTERVAL 10 MINUTE
+ORDER BY event_time DESC
