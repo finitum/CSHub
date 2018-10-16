871cd6b7368f5bfcff8c3b3bbf181f9cbe069b5b
RobbinBaauw <robbin.baauw@gmail.com>
2018-10-16 20:02:26 +0200

    The server will run on 80, internal only

--- faq-site-server/src/index.ts
+++ faq-site-server/src/index.ts
@@ -44,26 +44,8 @@ import "./auth/middleware";
 
 let server: http.Server | https.Server;
 
-if (Settings.LIVE) {
-
-    // Read the public and private keys
-    const options = {
-        key: fs.readFileSync("certs/csedelft-privkey.pem"),
-        cert: fs.readFileSync("certs/csedelft-pubkey.pem")
-    };
-    // Run the server on port 443 if live
-    server = https.createServer(options, app).listen(443);
-
-    // Use port 80 (http), but redirect to 443 (https)
-    http.createServer((req, res) => {
-        res.writeHead(301, {Location: `https://${req.headers.host}${req.url}`});
-        res.end();
-    }).listen(80);
-
-} else {
-    // Run the server on port 3000 if running local
-    server = http.createServer(app).listen(3000);
-}
+// Run the server on port 3000 if running local
+server = http.createServer(app).listen(3000);
 
 // Here all the connectors will be defined
 import "./components";
