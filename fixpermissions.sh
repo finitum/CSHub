2e814e2256bd1a7a31b13b9f000a58f47354cd2b
Robbin Baauw <robbin.baauw@gmail.com>
2018-10-17 17:09:28 +0000

    Fixed more permissions and added script

---
+++ fixpermissions.sh
@@ -0,0 +1,10 @@
+chown pm2:faqsite ./faq-site-server -R
+chown pm2:faqsite ./faq-site-shared -R
+chown victor:faqsite ./faq-site-client -R
+
+chmod 770 ./faq-site-server -R
+chmod 770 ./faq-site-shared -R
+chmod 771 ./faq-site-client -R
+
+chmod 700 ./faq-site-server/src/settings.*
+chmod 700 ./faq-site-server/src/auth/jwt-key.*
