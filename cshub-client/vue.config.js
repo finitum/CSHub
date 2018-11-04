33c3c071cc01ee59080490c93abed85cb2042895
Victor <victor@xirion.net>
2018-11-04 16:26:31 +0100

    Updating stuff for PWA/Service Worker

---
+++ cshub-client/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    themeColor: '#00A6D8',
+    name: 'CSHub'
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
