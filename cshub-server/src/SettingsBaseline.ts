33e83218028527a984b52f09846d30586608929c
Victor Roest <victor@xirion.net>
2019-01-15 15:18:26 +0100

    Added option to set settings through env vars as a precursor to the docker image

--- cshub-server/src/SettingsBaseline.ts
+++ cshub-server/src/SettingsBaseline.ts
@@ -39,38 +39,38 @@ export interface ISettings {
 }
 
 export const Settings: ISettings = {
-    LIVE: false,
-    PORT: 3000,
+    LIVE: process.env.LIVE ? process.env.LIVE === "true" : false,
+    PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
     DATABASE: {
-        HOST: "localhost",
-        USER: "xxx",
-        PASSWORD: "xxx",
-        NAME: "CSHubTest"
+        HOST: process.env.DATABASE_HOST ? process.env.DATABASE_HOST : "localhost",
+        USER: process.env.DATABASE_USER ? process.env.DATABASE_USER : "xxx",
+        PASSWORD: process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : "xxx",
+        NAME: process.env.DATABASE_NAME ? process.env.DATABASE_NAME : "CSHubTest",
     },
-    USESSH: true,
+    USESSH: process.env.USESSH ? process.env.USESSH === "true" : true,
     SSH: {
-        HOST: "xxx",
-        USER: "xxx",
-        PORT: 0,
-        PRIVATEKEYLOCATION: "xxx"
+        HOST: process.env.SSH_HOST ? process.env.SSH_HOST : "cshub",
+        USER: process.env.SSH_USER ? process.env.SSH_USER : "xxx",
+        PORT: process.env.SSH_PORT ? Number(process.env.SSH_PORT) : 22,
+        PRIVATEKEYLOCATION: process.env.SSH_PRIVATEKEYLOCATION ? process.env.SSH_PRIVATEKEYLOCATION : "xxx",
     },
-    DOMAIN: "192.168.xxx",
-    SITEADDRESS: "http://192.168.xxx",
-    TOKENAGEMILLISECONDS: 7200000,
-    PASSWORDITERATIONS: 42424,
-    JWTHASH: "xxxxx",
-    PASSWORDSALT: "xxxxx",
+    DOMAIN: process.env.DOMAIN ? process.env.DOMAIN : "192.168.x.x",
+    SITEADDRESS: process.env.SITEADDRESS ? process.env.SITEADDRESS : "http://192.168.xxx",
+    TOKENAGEMILLISECONDS: process.env.TOKENAGEMILLISECONDS ? Number(process.env.TOKENAGEMILLISECONDS) : 7200000,
+    PASSWORDITERATIONS: process.env.PASSWORDITERATIONS ? Number(process.env.PASSWORDITERATIONS) : 42424,
+    JWTHASH: process.env.JWTHASH ? process.env.JWTHASH : "xxxxx",
+    PASSWORDSALT: process.env.PASSWORDSALT ? process.env.PASSWORDSALT : "xxxxx",
     MAIL: {
-        USEGMAIL: false,
+        USEGMAIL: process.env.MAIL_USEGMAIL ? process.env.MAIL_USEGMAIL === "true" : false,
         GMAILSETTINGS: {
-            PASSWORD: "xxxx",
-            MAILADDRESS: "xxx@gmail.com"
+            PASSWORD: process.env.MAIL_GMAILSETTINGS_PASSWORD ? process.env.MAIL_GMAILSETTINGS_PASSWORD : "xxxx",
+            MAILADDRESS: process.env.MAIL_GMAILSETTINGS_MAILADDRESS ? process.env.MAIL_GMAILSETTINGS_MAILADDRESS : "xxx@gmail.com"
         },
-        APIKEY: "xxxx",
-        NOREPLYADDRESS: "no-reply@xxx.nl",
-        SUFFIX: "@xxxxx",
-        DEBUGMAILADDRESS: "xxxx",
-        VERIFYMAILADDRESSPREFIX: "xxx"
+        APIKEY: process.env.MAIL_APIKEY ? process.env.MAIL_APIKEY : "xxxx",
+        NOREPLYADDRESS: process.env.MAIL_APIKEY ? process.env.MAIL_APIKEY : "no-reply@xxx.nl",
+        SUFFIX: process.env.MAIL_SUFFIX ? process.env.MAIL_SUFFIX : "@xxxxx",
+        DEBUGMAILADDRESS: process.env.MAIL_DEBUGMAILADDRESS ? process.env.DEBUGMAILADDRESS : "xxxx",
+        VERIFYMAILADDRESSPREFIX: process.env.MAIL_VERIFYMAILADDRESSPREFIX ? process.env.VERIFYMAILADDRESSPREFIX : "xxxx"
     },
-    LOGLEVEL: "info"
+    LOGLEVEL: process.env.LOGLEVEL ? process.env.LOGLEVEL : "info"
 };
