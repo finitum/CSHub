221c751c64415160b485a3a8714924bedf9a3166
Victor <victor@xirion.net>
2019-01-17 14:07:32 +0100

    version.js all the things

---
+++ utilities/version.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const { readFileSync, writeFileSync } = require('fs');
+const { execSync } = require('child_process');
+
+const pkgFiles = ["cshub-client/package.json", "cshub-server/package.json", "cshub-shared/package.json"]
+
+const gitSHA = execSync("git rev-parse HEAD").toString().replace(/\s/g, '');
+console.log("Git SHA: " + gitSHA);
+
+for (const pkgFile of pkgFiles) {
+    const pkg = JSON.parse(readFileSync(pkgFile));
+    pkg["gitSHA"] = gitSHA;
+    writeFileSync(pkgFile, JSON.stringify(pkg));
+}
