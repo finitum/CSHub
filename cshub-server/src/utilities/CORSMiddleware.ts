071fcb3901459f3fc51f79e20f6a5945e88268ca
RobbinBaauw <robbin.baauw@gmail.com>
2019-02-02 21:02:44 +0100

    WWW redirect to root

--- cshub-server/src/utilities/CORSMiddleware.ts
+++ cshub-server/src/utilities/CORSMiddleware.ts
@@ -5,11 +5,7 @@ export const CORSMiddleware = (req: Request, res: Response, next: Function) => {
 
     // Add some headers so we don't have to deal with CORS problems
     res.header("Access-Control-Allow-Credentials", "true");
-    if (req.hostname.startsWith(`www`)) {
-        res.header("Access-Control-Allow-Origin", `${Settings.SITEPROTOCOL}://www.${Settings.SITEADDRESS}`);
-    } else {
-        res.header("Access-Control-Allow-Origin", `${Settings.SITEPROTOCOL}://${Settings.SITEADDRESS}`);
-    }
+    res.header("Access-Control-Allow-Origin", `${Settings.SITEPROTOCOL}://${Settings.SITEADDRESS}`);
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, version");
 
