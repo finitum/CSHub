14f08f01a2a691e439f8959b3e431cb2b2f56754
Robbin Baauw <robbin.baauw@gmail.com>
2019-04-26 10:06:04 +0200

    Removed authorization from unverified posts

--- cshub-server/src/components/pages/admin/GetUnverifiedPosts.ts
+++ cshub-server/src/components/pages/admin/GetUnverifiedPosts.ts
@@ -9,40 +9,34 @@ import {
 
 app.post(GetUnverifiedPosts.getURL, (req: Request, res: Response) => {
 
-    const token = checkTokenValidity(req);
+    query(`
+      SELECT hash, editCount
+      FROM posts T1
+             LEFT JOIN (
+        SELECT COUNT(*) AS editCount, post, MAX(datetime) AS datetime
+        FROM edits
+        GROUP BY post
+      ) T2 ON T1.id = T2.post
+      WHERE ((
+               SELECT approved
+               FROM edits X1
+               WHERE X1.post = T1.id
+               ORDER BY DATETIME DESC
+               LIMIT 1
+             ) = 0 OR editCount = 0 OR editCount IS NULL) AND T1.deleted = 0
+      GROUP BY T2.post
+      ORDER BY T1.datetime DESC
+    `)
+        .then((result: DatabaseResultSet) => {
 
-    if (token.valid) {
-        query(`
-          SELECT hash, editCount
-          FROM posts T1
-                 LEFT JOIN (
-            SELECT COUNT(*) AS editCount, post, MAX(datetime) AS datetime
-            FROM edits
-            GROUP BY post
-          ) T2 ON T1.id = T2.post
-          WHERE ((
-                   SELECT approved
-                   FROM edits X1
-                   WHERE X1.post = T1.id
-                   ORDER BY DATETIME DESC
-                   LIMIT 1
-                 ) = 0 OR editCount = 0 OR editCount IS NULL) AND T1.deleted = 0
-          GROUP BY T2.post
-          ORDER BY T1.datetime DESC
-        `)
-            .then((result: DatabaseResultSet) => {
+            const hashes: GetUnverifiedPostsType[] = [];
 
-                const hashes: GetUnverifiedPostsType[] = [];
-
-                result.convertRowsToResultObjects().forEach((x) => {
-                    hashes.push({
-                        hash: x.getNumberFromDB("hash")
-                    });
+            result.convertRowsToResultObjects().forEach((x) => {
+                hashes.push({
+                    hash: x.getNumberFromDB("hash")
                 });
-
-                res.json(new GetUnverifiedPostsCallBack(hashes));
             });
-    } else {
-        res.status(401).send();
-    }
+
+            res.json(new GetUnverifiedPostsCallBack(hashes));
+        });
 });
