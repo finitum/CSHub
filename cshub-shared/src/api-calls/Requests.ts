4447f1bcdd2b70d1b57e3cb86753a57c02601511
Victor Roest <victor@xirion.net>
2019-04-27 15:18:31 +0200

    Ordered the Requests file

--- cshub-shared/src/api-calls/Requests.ts
+++ cshub-shared/src/api-calls/Requests.ts
@@ -1,27 +1,31 @@
 export class Requests {
-    public static readonly LOGINREQUEST: string = "/login";
-    public static readonly CREATEACCOUNTREQUEST: string = "/createaccount";
-    public static readonly TOPICPOSTS: string = "/topicposts";
     public static readonly POSTDATA: string = "/post/:hash";
-    public static readonly POSTCONTENT: string = "/postcontent";
-    public static readonly TOPICS: string = "/topics";
-    public static readonly VERIFYTOKEN: string = "/verifytoken";
-    public static readonly SQUASHEDITS: string = "/squashedits";
-    public static readonly VERIFYMAIL: string = "/verifymail";
-    public static readonly SEARCH: string = "/search";
-    public static readonly FORGOTPASSWORDMAIL: string = "/forgotpasswordmail";
-    public static readonly FORGOTPASSWORD: string = "/forgotpassword";
-    public static readonly DASHBOARD: string = "/dashboard";
-    public static readonly CHANGEPASSWORD: string = "/changepassword";
-    public static readonly CHANGEAVATAR: string = "/changeavatar";
     public static readonly SUBMITPOST: string = "/submitpost";
+    public static readonly POSTCONTENT: string = "/postcontent";
+    public static readonly SQUASHEDITS: string = "/squashedits";
     public static readonly EDITPOST: string = "/editpost";
     public static readonly FORCEEDITPOST: string = "/forceeditpost";
     public static readonly EDITCONTENT: string = "/editcontent";
-    public static readonly SUBMITTOPIC: string = "/createtopic";
-    public static readonly GETALLUSERS: string = "/allusers";
+    public static readonly POSTSETTINGS: string = "/postsettings";
     public static readonly GETUNVERIFIEDPOSTS: string = "/unverifiedposts";
     public static readonly WIPPOSTS: string = "/wipposts";
-    public static readonly POSTSETTINGS: string = "/postsettings";
+
+    public static readonly TOPICS: string = "/topics";
+    public static readonly TOPICPOSTS: string = "/topicposts";
+    public static readonly SUBMITTOPIC: string = "/createtopic";
+
+    public static readonly LOGINREQUEST: string = "/login";
+    public static readonly CREATEACCOUNTREQUEST: string = "/createaccount";
+    public static readonly VERIFYTOKEN: string = "/verifytoken";
+
+    public static readonly GETALLUSERS: string = "/allusers";
+    public static readonly VERIFYMAIL: string = "/verifymail";
+    public static readonly FORGOTPASSWORDMAIL: string = "/forgotpasswordmail";
+    public static readonly FORGOTPASSWORD: string = "/forgotpassword";
+    public static readonly CHANGEPASSWORD: string = "/changepassword";
+    public static readonly CHANGEAVATAR: string = "/changeavatar";
+
+    public static readonly DASHBOARD: string = "/dashboard";
     public static readonly PROFILE = "/profile";
+    public static readonly SEARCH: string = "/search";
 }
