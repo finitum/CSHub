628389e236105565225caf8a7dae8583aa7ef681
Robbin Baauw <R.W.Baauw@student.tudelft.nl>
2019-05-04 15:58:39 +0200

    Restored deleted code (by Victor ofc)

--- cshub-server/src/components/pages/EditPost.ts
+++ cshub-server/src/components/pages/EditPost.ts
@@ -24,64 +24,95 @@ app.put(EditPost.getURL, (req: Request, res: Response) => {
         const userIsAdmin = userObj.tokenObj.user.admin;
 
         if (userIsAdmin) {
-            return query(`
-              SELECT content, approved
-              FROM edits
-              WHERE post = (
-                SELECT id
-                FROM posts
-                WHERE hash = ?
-              )
-              ORDER BY datetime ASC
-            `, postHash)
-                .then((edits: DatabaseResultSet) => {
-                    const rows = edits.convertRowsToResultObjects();
-                    const lastRow = rows[rows.length - 1];
-                    if (lastRow.getNumberFromDB("approved") !== 0) {
-                        res.sendStatus(204);
-                    } else {
-                        let delta = new Delta(JSON.parse(rows[0].getStringFromDB("content")));
+
+            if (editPostRequest.deleteEdit) {
+                return query(`
+                    DELETE T1
+                    FROM editusers T1
+                             INNER JOIN edits T2 on T1.edit = T2.id
+                    WHERE T2.post = (
+                        SELECT id
+                        FROM posts
+                        WHERE hash = ?
+                    )
+                      AND T2.approved = 0
+                `, postHash)
+                    .then(() => {
+                        return query(`
+                            DELETE
+                            FROM edits
+                            WHERE post = (
+                                SELECT id
+                                FROM posts
+                                WHERE hash = ?
+                            )
+                              AND approved = 0
+                        `, postHash);
+
+                    })
+                    .then((edits: DatabaseResultSet) => {
+                        res.sendStatus(200);
+                    });
+            } else {
+                return query(`
+                    SELECT content, approved
+                    FROM edits
+                    WHERE post = (
+                        SELECT id
+                        FROM posts
+                        WHERE hash = ?
+                    )
+                    ORDER BY datetime ASC
+                `, postHash)
+                    .then((edits: DatabaseResultSet) => {
+                        const rows = edits.convertRowsToResultObjects();
+                        const lastRow = rows[rows.length - 1];
+                        if (lastRow.getNumberFromDB("approved") !== 0) {
+                            res.sendStatus(204);
+                        } else {
+                            let delta = new Delta(JSON.parse(rows[0].getStringFromDB("content")));
 
                             for (let i = 1; i < rows.length; i++) {
                                 const currRow = rows[i];
                                 delta = delta.compose(new Delta(JSON.parse(currRow.getStringFromDB("content"))));
                             }
 
-                        getHTMLFromDelta(delta, (html, indexWords) => {
-                            query(`
-                              UPDATE edits, posts
-                              SET edits.approved    = 1,
-                                  edits.approvedBy  = ?,
-                                  edits.htmlContent = ?,
-                                  edits.indexWords  = ?,
-                                  edits.datetime    = NOW(),
-                                  posts.postVersion = posts.postVersion + 1,
-                                  posts.title       = ?,
-                                  posts.topic       = (SELECT id
-                                                       FROM topics
-                                                       WHERE hash = ?)
-                              WHERE edits.post = (
-                                SELECT id
-                                FROM posts
-                                WHERE hash = ?
-                              )
-                                AND edits.approved = 0
-                                AND posts.hash = ?
-                              ORDER BY edits.datetime DESC
-                              LIMIT 1
-                            `, userObj.tokenObj.user.id, html, indexWords, editPostRequest.postTitle, editPostRequest.postTopicHash, postHash, postHash)
-                                .then(() => {
-                                    logger.info("Edited post succesfully");
-                                    res.sendStatus(200);
-                                });
-                        });
-                    }
-                })
-                .catch(err => {
-                    logger.error(`Editing failed`);
-                    logger.error(err);
-                    res.status(500).send();
-                });
+                            getHTMLFromDelta(delta, (html, indexWords) => {
+                                query(`
+                                    UPDATE edits, posts
+                                    SET edits.approved    = 1,
+                                        edits.approvedBy  = ?,
+                                        edits.htmlContent = ?,
+                                        edits.indexWords  = ?,
+                                        edits.datetime    = NOW(),
+                                        posts.postVersion = posts.postVersion + 1,
+                                        posts.title       = ?,
+                                        posts.topic       = (SELECT id
+                                                             FROM topics
+                                                             WHERE hash = ?)
+                                    WHERE edits.post = (
+                                        SELECT id
+                                        FROM posts
+                                        WHERE hash = ?
+                                    )
+                                      AND edits.approved = 0
+                                      AND posts.hash = ?
+                                    ORDER BY edits.datetime DESC
+                                    LIMIT 1
+                                `, userObj.tokenObj.user.id, html, indexWords, editPostRequest.postTitle, editPostRequest.postTopicHash, postHash, postHash)
+                                    .then(() => {
+                                        logger.info("Edited post succesfully");
+                                        res.sendStatus(200);
+                                    });
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        logger.error(`Editing failed`);
+                        logger.error(err);
+                        res.status(500).send();
+                    });
+            }
         } else {
             res.status(401).send();
         }
