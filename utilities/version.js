570c25bd8e85de5877ad88f3e1b3770bf08ecae5
Victor Roest <victor@xirion.net>
2019-05-19 14:15:44 +0200

    Added ci

--- utilities/version.js
+++ utilities/version.js
@@ -3,7 +3,7 @@
 const { readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
-const pkgFiles = ["cshub-client/package.json", "cshub-server/package.json", "cshub-shared/package.json"]
+const pkgFiles = ["cshub-client/package.json", "cshub-server/package.json", "cshub-shared/package.json", "delta2html/package.json"];
 
 const gitSHA = execSync("git rev-parse HEAD").toString().replace(/\s/g, '');
 console.log("Git SHA: " + gitSHA);
